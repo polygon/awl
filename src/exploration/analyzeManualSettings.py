90a67422dec75830231b140de750171230291ec5
Polygon <polygon@wh2.tu-dresden.de>
2011-05-15 17:37:29 +0200

    New samples for analysis of automatic settings

--- src/exploration/analyzeManualSettings.py
+++ src/exploration/analyzeManualSettings.py
@@ -7,32 +7,32 @@ Created on Fri May  6 21:52:36 2011
 
 import scikits.audiolab as al
 import matplotlib.pyplot as pl
-from numpy import *
+import numpy as np
 import analysistools as tools
 
 wavs = [];
-wavs.append(al.wavread("Samples/Ch1_G1_M1_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M1.3_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M1.7_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M2_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M2.5_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M3.2_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M4_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M5_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M6.4_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M8_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M10_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M13_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M16_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M20_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M25_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M32_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M40_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M50_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M64_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M80_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M100_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
-wavs.append(al.wavread("Samples/Ch1_G1_M128_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M1_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M1.3_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M1.7_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M2_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M2.5_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M3.2_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M4_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M5_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M6.4_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M8_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M10_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M13_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M16_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M20_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M25_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M32_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M40_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M50_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M64_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M80_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M100_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M128_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
 #wavs.append(al.wavread("Samples/Ch1_G1_M128_G2_M128_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
 #wavs.append(al.wavread("Samples/Ch1_G1_M128_G3_M128_NoMaster_NoFlashPresent_sb900.wav")[0][:,0])
 #wavs.append(al.wavread("Samples/Ch1_G2_M128_G3_M128_NoMaster_NoFlashPresent_sb900.wav")[0][:,0])
@@ -41,6 +41,19 @@ wavs.append(al.wavread("Samples/Ch1_G1_M128_NoMaster_NoFlashPresent_d7000.wav")[
 #wavs.append(al.wavread("Samples/Ch1_G1_M32_G2_M32_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
 #wavs.append(al.wavread("Samples/Ch1_G2_M1_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
 #wavs.append(al.wavread("Samples/Ch1_G1_M1_G2_M1_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_TTL-3.0_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+#wavs.append(al.wavread("Samples/Ch1_G1_TTL-2.7_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
+#wavs.append(al.wavread("Samples/Ch1_G1_TTL_0_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
+#wavs.append(al.wavread("Samples/Ch1_G1_TTL+3.0_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
+#wavs.append(al.wavread("Samples/Ch1_G1_AA-3.0_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
+#wavs.append(al.wavread("Samples/Ch1_G1_AA_0_NoMaster_NoFlashPresent_d7000.wav")[0][:,0])
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_AA+3.0_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_TTL_0_G2_TTL_0_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_TTL_0_G2_TTL_0_G3_TTL_0_NoMaster_NoFlashPresent_d7000.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M32_G2_TTL_0_G3_TTL_0_NoMaster_NoFlashPresent_sb900.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M32_G2_M32_G3_TTL_0_NoMaster_NoFlashPresent_sb900.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_M32_G2_TTL_0_G3_M32_NoMaster_NoFlashPresent_sb900.wav")[0][:,0]))
+wavs.append(tools.normalize(al.wavread("Samples/Ch1_G1_TTL_0_G2_M32_G3_M32_NoMaster_NoFlashPresent_sb900.wav")[0][:,0]))
 N = len(wavs);
 
 # Extract the second chunk of every data block
@@ -50,6 +63,9 @@ td2 = [tools.findTdiffs(bl) for bl in bl2]
 pl2 = [tools.findPulses(bl) for bl in bl2]
 plnorm = [p - p[0] for p in pl2]
 
+pl.plot(wavs[-1])
+pl.show()
+
 # Group pulses
 corr = tools.groupPulses(plnorm)
 
@@ -57,6 +73,6 @@ corr = tools.groupPulses(plnorm)
 pl.figure()
 pl.hold(True)
 for i in range(N):
-    plot(corr[i], i * np.ones(len(corr[i])), 'bx')
+    pl.plot(corr[i], i * np.ones(len(corr[i])), 'bx')
     
 pl.show()
